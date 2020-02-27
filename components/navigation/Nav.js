cfb806763e2d26b9684577c113cb8de57abe35a6
ZeBalas <pedrotorre5@hotmail.com>
2020-02-27 17:39:51 +0000

    1 file modified

--- components/navigation/Nav.js
+++ components/navigation/Nav.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { 
   AppBar,
   Toolbar,
-  IconButton
+  IconButton,
+  Typography,
+  Button
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu'
 import SideDrawer from './SideDrawer'
@@ -16,11 +18,14 @@ const Nav = () => {
     setOpen(false);
   }
   return (
-    <AppBar>
+    <AppBar position="static">
       <Toolbar>
         <IconButton onClick={handleOpen} color="secondary">
           <MenuIcon />
         </IconButton>
+        <Button color="secondary">
+          Login
+        </Button>
       </Toolbar>
       <SideDrawer open={open} handleClose={handleClose}/>
     </AppBar>
